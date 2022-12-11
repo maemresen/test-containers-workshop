bddb199741488d257f60c4ed3ac68a97ec6ca2b4
Mehmet Arif Emre Şen <maemresen@yazilim.vip>
2022-12-11 19:39:06 +0300

    spring boot example module created

---
+++ test-containers-workshop-spring-boot/src/main/java/com/maemresen/test/containers/workhop/spring/boot/TestcontainersWorkshopSpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.maemresen.test.containers.workhop.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Emre Şen (maemresen@yazilim.vip), 11/12/2022
+ */
+@SpringBootApplication
+public class TestcontainersWorkshopSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestcontainersWorkshopSpringBootApplication.class, args);
+    }
+}
