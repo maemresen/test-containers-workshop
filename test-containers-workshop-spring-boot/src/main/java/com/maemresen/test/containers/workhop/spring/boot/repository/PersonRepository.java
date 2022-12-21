f8ebbd92c425d02f6f6e5d3b32ede8348ac5ed36
Mehmet Arif Emre Şen <maemresen@yazilim.vip>
2022-12-21 14:11:10 +0300

    postgresql configured to working with spring

--- test-containers-workshop-spring-boot/src/main/java/com/maemresen/test/containers/workhop/spring/boot/repository/PersonRepository.java
+++ test-containers-workshop-spring-boot/src/main/java/com/maemresen/test/containers/workhop/spring/boot/repository/PersonRepository.java
@@ -1,10 +1,13 @@
 package com.maemresen.test.containers.workhop.spring.boot.repository;
 
 import com.maemresen.test.containers.workhop.spring.boot.domain.Person;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * @author Emre Şen (maemresen@yazilim.vip), 11/12/2022
  */
 public interface PersonRepository extends JpaRepository<Person, Integer> {
+
+    Optional<Person> findTopByNameIgnoreCase(String name);
 }
--- test-containers-workshop-spring-boot/src/test/java/com/maemresen/test/containers/workhop/spring/boot/service/impl/PersonServiceImplTest.java
+++ test-containers-workshop-spring-boot/src/test/java/com/maemresen/test/containers/workhop/spring/boot/service/impl/PersonServiceImplTest.java
@@ -3,25 +3,44 @@ package com.maemresen.test.containers.workhop.spring.boot.service.impl;
 import com.maemresen.test.containers.workhop.spring.boot.domain.Person;
 import com.maemresen.test.containers.workhop.spring.boot.repository.PersonRepository;
 import java.util.List;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 /**
  * @author Emre Şen (maemresen@yazilim.vip), 11/12/2022
  */
+@Slf4j
 @RunWith(SpringRunner.class)
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest
 public class PersonServiceImplTest {
 
+    @ClassRule
+    public static final PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:15.1")
+        .withDatabaseName("integration-tests-db")
+        .withUsername("sa")
+        .withPassword("sa");
+
+    @DynamicPropertySource
+    public static void overrideProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create");
+    }
+
     @Autowired
-    PersonRepository personRepository;
+    private PersonRepository personRepository;
 
     @Transactional
     @Test
@@ -32,14 +51,34 @@ public class PersonServiceImplTest {
         personRepository.save(Person.builder().name("Sophia").surname("Martinez").build());
 
         List<Person> allPeople = personRepository.findAll();
+        Optional<Person> emre = personRepository.findTopByNameIgnoreCase("emre");
 
+        Assertions.assertTrue(emre.isPresent());
         Assertions.assertNotNull(allPeople);
         Assertions.assertEquals(4, allPeople.size());
     }
 
-//    @Test
-//    public void test_findAllDatabaseUnreachable() {
-//        postgreSQLContainer.stop();
-//        Assertions.assertThrows(Throwable.class, personRepository::findAll);
-//    }
+    @Transactional
+    @Test
+    public void test_findByNameCaseSensitive() {
+        personRepository.save(Person.builder().name("Emre").surname("Sen").build());
+        Optional<Person> emre = personRepository.findTopByNameIgnoreCase("Emre");
+        Assertions.assertTrue(emre.isPresent());
+    }
+
+    @Transactional
+    @Test
+    public void test_findByNameCaseInSensitive() {
+        personRepository.save(Person.builder().name("Emre").surname("Sen").build());
+        Optional<Person> emre = personRepository.findTopByNameIgnoreCase("emre");
+        Assertions.assertTrue(emre.isPresent());
+    }
+
+    @Transactional
+    @Test
+    public void test_findByNameNotFound() {
+        personRepository.save(Person.builder().name("Emre").surname("Sen").build());
+        Optional<Person> emre = personRepository.findTopByNameIgnoreCase("eren");
+        Assertions.assertTrue(emre.isEmpty());
+    }
 }
