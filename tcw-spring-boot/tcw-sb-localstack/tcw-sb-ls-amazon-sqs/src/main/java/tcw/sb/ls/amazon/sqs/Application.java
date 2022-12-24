3106128b38156071b55edbdd5cde3fa0f141259a
Mehmet Arif Emre Şen <maemresen@yazilim.vip>
2022-12-24 04:36:49 +0300

    localstack module added

---
+++ tcw-spring-boot/tcw-sb-localstack/tcw-sb-ls-amazon-sqs/src/main/java/tcw/sb/ls/amazon/sqs/Application.java
@@ -0,0 +1,15 @@
+package tcw.sb.ls.amazon.sqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Emre Şen (maemresen@yazilim.vip), 24/12/2022
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
